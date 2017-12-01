884d4850d3aeee9f5b8b568cb46557f22c95c834
Riki Montgomery <mostriki820@gmail.com>
2017-12-01 14:50:44 -0800

    fix .each loop in index and display projects in a more readable list

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,49 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Project.all
-    render :index
+    @projects = Project.all.limit(6)
   end
 
   def show
     @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+  #
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "You've added #{@project.title} to your projects."
+      redirect_to  projects_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+     @project = Project.find(params[:id])
+   end
+
+  def update
+    @project= Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to projects_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    if @project.destroy
+      flash[:alert] = "You've deleted the #{@project.title} project."
+      redirect_to projects_path
+    end
+  end
+
+private
+  def project_params
+    params.require(:project).permit(:title, :header, :url)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ Project.destroy_all
 projects = []
 
 10.times do |index|
-  projects << Project.create!(title: Faker::ChuckNorris.fact, header: Faker::Lorem.words(4), url: Faker::Internet.url)
+  projects << Project.create!(title: Faker::ChuckNorris.fact, header: Faker::Lorem.sentence, url: Faker::Internet.url)
 end
