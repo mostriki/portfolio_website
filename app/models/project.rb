fea7d531c4884bf847593e251f4dd8cd21e230b4
Riki Montgomery <mostriki820@gmail.com>
2017-12-01 13:48:08 -0800

    create scope to display 5 most recent

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ActiveRecord::Base
   validates :title, :presence => true
   validates :header, :presence => true
   validates :url, :presence => true
+
+  scope :top_five_most_recent, -> { order(created_at: :desc).limit(10) }
 end
