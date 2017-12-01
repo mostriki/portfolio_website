5bd4b75fa00e371f8b9c636c09fbd7716819f320
Riki Montgomery <mostriki820@gmail.com>
2017-12-01 15:20:09 -0800

    add blog controller and CRUD

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,49 @@
 class BlogsController < ApplicationController
   def index
-    @blogs = Blog.all
-    render :index
+    @blogs = Blog.all.limit(5)
   end
 
   def show
     @blog = Blog.find(params[:id])
+  end
+
+  def new
+    @blog = Blog.new
+  end
+  #
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      flash[:notice] = "You've added #{@blog.title} to your blogs."
+      redirect_to  blogs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+     @blog = Blog.find(params[:id])
+   end
+
+  def update
+    @blog= Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    if @blog.destroy
+      flash[:alert] = "You've deleted the #{@blog.title} blog."
+      redirect_to blogs_path
+    end
+  end
+
+private
+  def blog_params
+    params.require(:blog).permit(:title, :header, :url)
   end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,6 @@ class Blog < ActiveRecord::Base
   validates :title, :presence => true
   validates :header, :presence => true
   validates :body, :presence => true
+
+  scope :top_five_most_recent, -> { order(created_at: :desc).limit(5) }
 end
