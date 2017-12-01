c1cf64cd2a68d51951c0f4e04ae0ff38f315054a
Riki Montgomery <mostriki820@gmail.com>
2017-12-01 15:48:26 -0800

    seed the blog

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@ projects = []
 10.times do |index|
   projects << Project.create!(title: Faker::ChuckNorris.fact, header: Faker::Lorem.sentence, url: Faker::Internet.url)
 end
+
+Blog.destroy_all
+
+blogs = []
+
+10.times do |index|
+  blogs << Blog.create!( title: Faker::GameOfThrones.character, header: Faker::Lorem.sentence, body: Faker::Lorem.characters(51))
+end
