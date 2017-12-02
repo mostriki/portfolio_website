3cf05f8c87941681a4b46ce236f82fc899106bc4
Riki Montgomery <mostriki820@gmail.com>
2017-12-01 16:02:02 -0800

    add about, contact, and home pages to views.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root'pages#home'
 
-  get 'contact', to: 'pages#home'
+  get 'contact', to: 'pages#contact'
   get 'about', to: 'pages#about'
   get 'error', to: 'pages#error'
 
